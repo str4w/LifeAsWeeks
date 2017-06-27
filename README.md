32c3c535b088f3780052ffff95d4d1a3718ca024
str4w <strawdog3@gmail.com>
2017-06-27 07:35:54 -0400

    Very basic starting point, can generate a PDF with cairo, and put a few hard-coded points on the chart.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# LifeAsWeeks
-A generator for life calendars with time chunked into weeks. (Inspired by https://waitbutwhy.com/2014/05/life-weeks.html )
+# Life As Weeks
+Tim Urban of Wait but Why wrote a post discussing the week as a natural chunk in which to subdivide life.   https://waitbutwhy.com/2014/05/life-weeks.html It is a good idea - the week is a very natural unit - indeed the week based system has been in use far longer than our calendar.  He even provides some basic posters with 52 x 90 blocks that you can mark up.
+
+But I wanted to do something more sophisticated.  First of all, I'm lazy, and the idea of physically looking up dates and counting out the weeks seemed like a job for the computer to do.  Second, there are 52 weeks _plus one day_ in each year, except leap years which have two extra days.  Which is a minor difference, except when it's not.  After a couple decades, theres some missing weeks, and the week count will move around noticeably relative to the seasons.
+
+Thus was born the life as weeks generator.  Intended to take a list of dates, and some options, it generates a day-accurate chart of weeks, with the calendar overlaid.
+
+
---
+++ weeks.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 25 07:28:59 2017
+
+@author: rupert
+"""
+
+#!/usr/bin/env python
+
+import math
+import cairo
+import datetime
+
+
+if not cairo.HAS_PDF_SURFACE:
+    raise Exception('cairo was not compiled with PDF support')
+
+def dow(y,m,d):
+    return datetime.date(y,m,d).isoweekday()%7
+def daysbetween(a,b):
+    return (datetime.date(a[0],a[1],a[2])-datetime.date(b[0],b[1],b[2])).days
+
+def draw_func(ctx,w,h):
+    
+    aspect=float(h)/w
+
+    ctx.scale (min(w,h),min(w,h)) # Normalizing the canvas
+
+#    pat = cairo.LinearGradient (0.0, 0.0, 0.0, 1.0)
+#    pat.add_color_stop_rgba (1, 0.7, 0, 0, 0.5) # First stop, 50% opacity
+#    pat.add_color_stop_rgba (0, 0.9, 0.7, 0.2, 1) # Last stop, 100% opacity
+#
+#    ctx.rectangle (0, 0, 1, 1) # Rectangle(x0, y0, x1, y1)
+#    ctx.set_source (pat)
+#    ctx.fill ()
+    margin=0.05
+    top=margin
+    bottom=aspect-margin
+    grid=(0.05+margin,1.0-margin)
+    startyear=1973
+    verticalgap=0.003
+
+    nbrows=90
+    
+    spans=[[(1973,1,1),(1973,2,20), (0.5,0.5,0.5)],
+           [(1979,9,1),(1985,7,1),  (0.4,0.8,0.4)],
+           [(1985,9,1),(1991,7,1),  (0.4,0.4,0.8)],
+           [(1991,9,1),(1996,6,1),  (0.8,0.4,0.8)],
+            ]
+
+    cr.select_font_face("Sans", cairo.FONT_SLANT_NORMAL,
+                        cairo.FONT_WEIGHT_BOLD)
+    cr.set_font_size(0.01)
+
+    def faded():
+        ctx.set_source_rgb (0.5, 0.5, 0.5) # Solid color
+        ctx.set_line_width (0.001)
+    def dark():
+        ctx.set_source_rgb (0.0, 0.0, 0.0) # Solid color
+        ctx.set_line_width (0.001)
+    def month():
+        ctx.set_source_rgb (0.8, 0.0, 0.0) # Solid color
+        ctx.set_line_width (0.0005)
+
+    def xpos(day):
+        return grid[0]+(grid[1]-grid[0])*day/(54.*7)
+
+    def rowcenter(y):
+        totalheight=bottom-top+verticalgap
+        perrow=totalheight/nbrows
+        return top-verticalgap/2+perrow*(y+0.5)
+    def rowheight():
+        totalheight=bottom-top+verticalgap
+        perrow=totalheight/nbrows
+        return perrow-verticalgap
+    
+    def rowtop(y):
+        return rowcenter(y)-rowheight()/2.
+
+    def rowbottom(y):
+        return rowcenter(y)+rowheight()/2.
+    def getdayspan(year,span):
+        start=datetime.date(span[0][0],span[0][1],span[0][2])
+        end=datetime.date(span[1][0],span[1][1],span[1][2])
+        ystart=datetime.date(year,1,1)
+        yend=datetime.date(year+1,1,1)
+        din=(start-ystart).days
+        dend=(end-ystart).days
+        dyr=(yend-ystart).days
+        if dend<0 or din>dyr:
+            return None
+        else:
+            return (max(din,0),min(dyr,dend))
+
+    for y in range(0,nbrows):
+        year=startyear+y
+        startdow=0#dow(year,1,1)
+        for s in spans:
+            dayspan=getdayspan(year,s)
+            if not dayspan is None:
+                print "Setting",s,"for year",year
+                print dayspan
+                h0=rowtop(y) #top+(y-1)*(bottom-top)/nbrows
+                h1=rowbottom(y) #top+y*(bottom-top)/nbrows
+                x0=xpos(startdow+dayspan[0])
+                x1=xpos(startdow+dayspan[1])
+                ctx.rectangle (x0, h0, x1-x0, h1-h0)
+                ctx.set_source_rgb(s[2][0],s[2][1],s[2][2])
+                ctx.fill()
+
+
+
+
+    faded()
+    for y in range(0,nbrows):
+        h0=rowtop(y) #top+(y-1)*(bottom-top)/nbrows
+        h1=rowbottom(y) #top+y*(bottom-top)/nbrows
+        #h1=top+y*(bottom-top)/nbrows
+        ctx.move_to (grid[0], h0)
+        ctx.line_to (grid[1], h0)
+#        ctx.close_path ()
+        ctx.stroke ()
+        ctx.move_to (grid[0], h1)
+        ctx.line_to (grid[1], h1)
+#        ctx.close_path ()
+        ctx.stroke ()
+        for x in [grid[0],xpos(53*7),grid[1]]:
+            ctx.move_to (x,  h0)
+            ctx.line_to (x,  h1)
+            ctx.stroke ()
+
+    dark()
+#    for w in range(1,53):
+#        x0=xpos(w*7)
+#        ctx.move_to (x0, top)
+#        ctx.line_to (x0, bottom)
+#        ctx.stroke ()
+    for y in range(0,nbrows):
+        dark()
+        year=startyear+y
+        startdow=0#dow(year,1,1)
+        endday=startdow+daysbetween((year+1,1,1),(year,1,1))
+        x0=xpos(startdow)
+        x1=xpos(endday)
+        h0=rowtop(y) #top+(y-1)*(bottom-top)/nbrows
+        h1=rowbottom(y) #top+y*(bottom-top)/nbrows
+        ctx.move_to (0.05,h1)
+        ctx.show_text(str(year))
+        ctx.move_to (x0, h0)
+        ctx.line_to (x1, h0)
+        ctx.stroke ()
+        ctx.move_to (x0, h1)
+        ctx.line_to (x1, h1)
+        ctx.stroke ()
+        ctx.move_to (x0, h0)
+        ctx.line_to (x0, h1)
+        ctx.stroke ()
+        ctx.move_to (x1, h0)
+        ctx.line_to (x1, h1)
+        ctx.stroke ()
+        for d in range(1,daysbetween((year+1,1,1),(year,1,1))):
+            if (datetime.date(year,1,1)+datetime.timedelta(days=d)).weekday()==6:
+                wday=startdow+d
+                xw=xpos(wday)
+                ctx.move_to (xw, h0)
+                ctx.line_to (xw, h1)
+                ctx.stroke ()
+        month()
+        for m in range(2,13):
+            mday=startdow+daysbetween((year,m,1),(year,1,1))
+            xm=xpos(mday)
+            ctx.move_to (xm, h0)
+            ctx.line_to (xm, h1)
+            ctx.stroke ()
+            
+ #       ctx.close_path ()
+ #       ctx.stroke ()
+
+
+    #ctx.set_source_rgb (0.3, 0.2, 0.5) # Solid color
+    #ctx.set_line_width (0.02)
+
+
+
+
+width_in_inches, height_in_inches = 8.5, 11
+width_in_points, height_in_points = \
+        width_in_inches * 72, height_in_inches * 72
+width, height = width_in_points, height_in_points
+
+surface = cairo.PDFSurface("weeks.pdf",width,height)
+cr = cairo.Context (surface)
+cr.save()
+draw_func(cr, width, height)
+cr.restore()
+cr.show_page()
+surface.finish()
+
+
+
